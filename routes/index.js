d17588833f4ac06bbb5228fca0d2814334834b10
Andres Carrasco Q. <38218181+andritowmega@users.noreply.github.com>
2024-11-19 01:14:53 -0500

    feat ⚗️ test to automatic build

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'FastMVP' });
+  res.render('index', { title: 'FastMVP2' });
 });
 
 module.exports = router;
