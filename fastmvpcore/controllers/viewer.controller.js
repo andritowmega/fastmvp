d15afab5d27a0094a8d715d047bbb771a67c1dc8
Andres Carrasco Q. <38218181+andritowmega@users.noreply.github.com>
2024-12-25 22:39:09 -0500

    popovers and bootstrap template (#42)
    * popovers and bootstrap template
    
    * view tables contet

--- fastmvpcore/controllers/viewer.controller.js
+++ fastmvpcore/controllers/viewer.controller.js
@@ -1,28 +1,50 @@
-class ViewerController{
-    static async Projects(req,res){
-        let projects = require("../../config/configDb.json");
-        const projectsNames = Object.keys(projects);
-        return res.render('fmvp/index', { title: 'FastMVP View', projects:projectsNames });
-    }
-    static async SingleProject(req,res){
-        const {getTables} = require("../services/viewer.service");
-        const response = await getTables(req.params.project).catch((e) => {
-            console.error("FastMvp Controller: can't create", e);
-            return e;
-          });
-        console.log("getTables",response);
-        return res.render('fmvp/project/index', { title: req.params.project, tables:response.data });
-    }
+class ViewerController {
+  static async Projects(req, res) {
+    let projects = require("../../config/configDb.json");
+    const projectsNames = Object.keys(projects);
+    return res.render("fmvp/index", {
+      title: "FastMVP View",
+      projects: projectsNames,
+    });
+  }
+  static async SingleProject(req, res) {
+    const { getTables } = require("../services/viewer.service");
+    const response = await getTables(req.params.project).catch((e) => {
+      console.error("FastMvp Controller: can't create", e);
+      return e;
+    });
+    return res.render("fmvp/project/index", {
+      title: req.params.project,
+      tables: response.data,
+    });
+  }
 
-    static async GetTableInfo(req,res){
-        const {getMetaData} = require("../services/viewer.service");
-        const response = await getMetaData(req.params.project,req.params.table).catch((e) => {
-            console.error("FastMvp Controller: can't create", e);
-            return e;
-          });
-        console.log("getTable nfo",response.data);
-        return res.render('fmvp/project/table', { project: req.params.project,table:req.params.table , metadata:response.data });
-    }
-
+  static async GetTableInfo(req, res) {
+    const filtersGet = {};
+    const { getMetaData } = require("../services/viewer.service");
+    const response = await getMetaData(
+      req.params.project,
+      req.params.table
+    ).catch((e) => {
+      console.error("FastMvp Controller: can't create", e);
+      return e;
+    });
+    const { get } = require("../services/allfunctions.service");
+    const responseTable = await get(
+      req.params.project,
+      req.params.table,
+      filtersGet
+    ).catch((e) => {
+      console.error("FastMvp Controller: can't get", e);
+      return e;
+    });
+    console.log("table",responseTable.data);
+    return res.render("fmvp/project/table", {
+      project: req.params.project,
+      table: req.params.table,
+      metadata: response.data,
+      content: responseTable.data
+    });
+  }
 }
 module.exports = ViewerController;
