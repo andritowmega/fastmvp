1e86c0843e0f6d8460f7f5db590c2a7102726390
Andres Carrasco Q. <38218181+andritowmega@users.noreply.github.com>
2024-12-26 00:29:50 -0500

    refactor: ♻️ The start page was redesigned by creating the options menu for projects and documentation #44

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'FastMVP2' });
+  res.render('index', { title: 'FastMVP' });
+});
+router.get('/doc', function(req, res, next) {
+  res.render('doc', { title: 'Documentación' });
 });
 
 module.exports = router;
