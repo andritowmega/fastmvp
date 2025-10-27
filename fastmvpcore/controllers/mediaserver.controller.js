92b9c4458df12de2f4c16b15ef20baff70576156
Andritow <andrew_c_q@hotmail.com>
2025-10-26 23:44:27 -0500

    test :test_tube: 2 level files

--- fastmvpcore/controllers/mediaserver.controller.js
+++ fastmvpcore/controllers/mediaserver.controller.js
@@ -7,21 +7,23 @@ class MediaServerController {
       const { project, level1, level2, name } = req.params;
 
       const urlConfig = path.join(__dirname, "../../config/configDb.json");
-      const optionsConnetion = require(urlConfig);
+      const config = require(urlConfig);
 
-      // Verificar configuración
-      if (!optionsConnetion[project]?.files?.mp3?.free || optionsConnetion[project].files.mp3.free === "") {
+      if (!config[project]?.files?.mp3?.free) {
         return res.status(404).send("No hay rutas configuradas");
       }
 
-      // Construir ruta absoluta al archivo
-      const basePath = optionsConnetion[project].files.mp3.free;
-      const filePath = path.join(basePath, level1, level2, name);
+      const basePath = config[project].files.mp3.free;
+      let filePath;
 
-      // Verificar existencia
+      // Detectar qué parámetros existen
+      if (level1 && level2) {
+        filePath = path.join(basePath, level1, level2, name);
+      } else {
+        filePath = path.join(basePath, name);
+      }
+
       await fs.access(filePath, fs.constants.F_OK);
-
-      // Enviar el archivo
       res.setHeader("Content-Type", "audio/mpeg");
       return res.sendFile(filePath);
     } catch (err) {
